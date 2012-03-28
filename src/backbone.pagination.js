7ca78d88698ba831dd1708884167998d753217c9
Carl Licata <carl@carl.is>
2012-03-27 23:08:38 -0400

    Abstracting variable names (so they can easily be changed for use with other paginated APIs)

--- src/backbone.pagination.js
+++ src/backbone.pagination.js
@@ -1,23 +1,46 @@
-Backbone.PaginatedCollection = Backbone.Collection.extend({
-  initialize: function() {
-    this.page = 1;
-    this.pages = 1;
-    this.per_page = 10; // 10 is the default number of results per page
-    this.total_results = 0;
-  }
-});
+(function() {
+  var results_object_name = 'results',
+      pagination_object_name = 'pagination',
 
-_.extend(Backbone.PaginatedCollection.prototype, {
-  parse: function(response, request) {
-    this._updatePaginationInfo(response['pagination']);
+      pagination_object_map = {
+        page:           'page',
+        pages:          'page_count',
+        per_page:       'per_page',
+        total_results:  'result_count'
+      },
 
-    return response['results'];
-  },
+      pagination_default_values = {
+        page:           1,
+        pages:          1,
+        per_page:       10,
+        total_results:  0;
+      };
 
-  _updatePaginationInfo: function(info) {
-    this.page = info['page'];
-    this.pages = info['page_count'];
-    this.per_page = info['per_page'];
-    this.total_results = info['result_count'];
-  }
-});
+  Backbone.PaginatedCollection = Backbone.Collection.extend({
+    initialize: function() {
+      var self = this,
+          setDefaultValue = function(value, variable) {
+            self[variable] = value;
+          };
+
+      _(pagination_default_values).each(setDefaultValue);
+    }
+  });
+
+  _.extend(Backbone.PaginatedCollection.prototype, {
+    parse: function(response, request) {
+      this._updatePaginationInfo(response[pagination_object_name]);
+
+      return response[results_object_name];
+    },
+
+    _updatePaginationInfo: function(info) {
+      var self = this,
+          setPaginationVariable = function(response_variable, variable) {
+            self[variable] = info[response_variable];
+          };
+
+      _(pagination_object_map).each(setPaginationVariable);
+    }
+  });
+}).call(this);
